cc310af9b1e0ea157add5011b9ee28c124566651
Damien Gilles <damien.gilles@alterway.fr>
2018-10-10 12:10:05 +0200

    the plugin works better with qualification plugin and does not generate 500 if left unconfigured

--- lib/ponderation_hooks.rb
+++ lib/ponderation_hooks.rb
@@ -3,6 +3,10 @@ require 'net/http'
 require 'json'
 
 class PonderationHooks < Redmine::Hook::ViewListener
+    def controller_issues_new_before_save(context)
+        setPonderation(context)
+    end
+
     def controller_issues_new_before_save_after_qualification(context)
         setPonderation(context)
     end
@@ -12,39 +16,42 @@ class PonderationHooks < Redmine::Hook::ViewListener
     end
 
     def setPonderation(context)
-        ponderation = 0
 
-        Setting.plugin_ponderation['weights'].each do |key, value|
-            if key.match(/\d+/) # is a custom field
-                if value.is_a?(Hash) # the field is a selector
-                    Setting.plugin_ponderation['weights'][key].each do |skey, svalue|
-                        if skey === context[:params][:issue][:custom_field_values][key]
-                            ponderation += Setting.plugin_ponderation['weights'][key][skey].to_f
+        if Setting.plugin_ponderation['weights']
+            ponderation = 0
+
+            Setting.plugin_ponderation['weights'].each do |key, value|
+                if key.match(/\d+/) # is a custom field
+                    if value.is_a?(Hash) # the field is a selector
+                        Setting.plugin_ponderation['weights'][key].each do |skey, svalue|
+                            if skey === context[:params][:issue][:custom_field_values][key]
+                                ponderation += Setting.plugin_ponderation['weights'][key][skey].to_f
+                            end
                         end
-                    end
-                else
-                    ponderation += Setting.plugin_ponderation['weights'][key].to_f * context[:params][:issue][:custom_field_values][key].to_f
-                 end
-            else # is a default field
-                if value.is_a?(Hash) # the field is a selector
-                    Setting.plugin_ponderation['weights'][key].each do |skey, svalue|
-                        if skey.to_i === context[:issue][key].to_i
-                            ponderation += Setting.plugin_ponderation['weights'][key][skey].to_f
-                        end
-                    end
-                else
-                    if (key === 'created_on' && !context[:issue]['created_on']) || key === 'updated_on'
-                        ponderation += Setting.plugin_ponderation['weights'][key].to_f * Time.now.to_i
                     else
-                        ponderation += Setting.plugin_ponderation['weights'][key].to_f * context[:issue][key].to_f
+                        ponderation += Setting.plugin_ponderation['weights'][key].to_f * context[:params][:issue][:custom_field_values][key].to_f
+                    end
+                else # is a default field
+                    if value.is_a?(Hash) # the field is a selector
+                        Setting.plugin_ponderation['weights'][key].each do |skey, svalue|
+                            if skey.to_i === context[:issue][key].to_i
+                                ponderation += Setting.plugin_ponderation['weights'][key][skey].to_f
+                            end
+                        end
+                    else
+                        if (key === 'created_on' && !context[:issue]['created_on']) || key === 'updated_on'
+                            ponderation += Setting.plugin_ponderation['weights'][key].to_f * Time.now.to_i
+                        else
+                            ponderation += Setting.plugin_ponderation['weights'][key].to_f * context[:issue][key].to_f
+                        end
                     end
                 end
             end
-        end
 
-        custom_field_values = context[:params][:issue][:custom_field_values]
-        custom_field_values[Setting.plugin_ponderation['field_id']] = ponderation
-        
-        context[:issue].custom_field_values = custom_field_values
+            custom_field_values = context[:params][:issue][:custom_field_values]
+            custom_field_values[Setting.plugin_ponderation['field_id']] = ponderation
+            
+            context[:issue].custom_field_values = custom_field_values
+        end
     end
 end
