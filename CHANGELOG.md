06423d7630ca913a377a84474512732cc8330540
Damien Gilles <damien.gilles@alterway.fr>
2018-10-15 10:59:51 +0200

    updated version and changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Versions
+
+## 0.0.1
+
+- first working version
+
+## 0.1.1
+
+- The plugin won't crash when left unconfigured
--- init.rb
+++ init.rb
@@ -5,6 +5,6 @@ Redmine::Plugin.register :ponderation do
   name 'Ponderation plugin'
   author 'Damien GILLES'
   description "Customisable ticket's priority calcul (require qualification plugin)"
-  version '0.0.1'
+  version '0.1.1'
   settings partial: 'settings/ponderation', default: {}
 end
